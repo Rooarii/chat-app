6b7fed5653cbf2e647b9bc274d666c4e44b22ac1
Rooarii MANUEL <rooarii.manuel@gmail.com>
2020-10-11 23:05:53 +0200

    quest finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import './Contact';
+import './Contact.css';
+
 const name='Harold Crawford';
 const avatar = 'https://randomuser.me/api/portraits/men/79.jpg';
-// const connected = true;
+const connected = true;
 
 function Contact(){
  return (
-     <b className='Contact'>
-         <img src={avatar} alt={name}/>
-         <div>
-             <h3>{name}</h3>
-         </div>
-     </b>
+    <b className='Contact'>
+        <img class="avatar" src={avatar} alt={name}/>
+        <div className='status'>
+            <h4 className='name'>{name}</h4>
+            <div className='status-online'></div>
+            <p className='status-text'>{connected?'online':'offline'}</p>
+        </div>
+    </b>
  )
 };
 
